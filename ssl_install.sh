84e2fa55274bb1b94499440d95289fba59ac563b
jonathan mielke <jonathanmielke3d@gmail.com>
2024-01-29 14:07:02 -0700

    fix domain input

--- ssl_install.sh
+++ ssl_install.sh
@@ -4,5 +4,5 @@ sudo snap install --classic certbot
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
 sudo ufw allow 80
 sudo ufw allow 443
-read -p "Enter user: " domain
-sudo certbot certonly --standalone --config-dir /usr/local/bin/mediamtx/certbot -d domain
+read -p "Enter Domain: " domain
+sudo certbot certonly --standalone --config-dir /usr/local/bin/mediamtx/certbot -d $domain
