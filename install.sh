6cb40b8426516191a0ce00fa01b4a8f1ecb48ec0
jonathan mielke <jonathanmielke3d@gmail.com>
2024-01-29 13:50:33 -0700

    add ssl install script

--- install.sh
+++ install.sh
@@ -1,17 +1,9 @@
 sudo apt-get update -y
 sudo apt update && sudo apt upgrade -y
 sudo apt install ffmpeg -y
-sudo cp ./Other_Configs/mediamtx.service /etc/systemd/system/mediamtx.service
+sudo cp ./service/mediamtx.service /etc/systemd/system/mediamtx.service
 sudo systemctl daemon-reload
 sudo systemctl enable mediamtx
 sudo systemctl start mediamtx
 sudo ufw allow 8554/tcp && sudo ufw allow 8000/udp && sudo ufw allow 8001/udp && sudo ufw allow 1935/tcp && sudo ufw allow 8888:8889/tcp && sudo ufw allow 8889/tcp && sudo ufw allow 8890/udp && sudo ufw allow 8189/udp && sudo ufw allow 1936/tcp
 
-# mkdir /root/mediamtx
-# sudo snap install core; sudo snap refresh core
-# sudo snap install --classic certbot
-# sudo ln -s /snap/bin/certbot /usr/bin/certbot
-# sudo ufw allow 80
-# sudo ufw allow 443
-# todo add input request
-#sudo certbot certonly --standalone --config-dir /root/mediamtx -d your.domain
---
+++ ssl_install.sh
@@ -0,0 +1,8 @@
+mkdir /usr/local/bin/mediamtx/certbot
+sudo snap install core; sudo snap refresh core
+sudo snap install --classic certbot
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+sudo ufw allow 80
+sudo ufw allow 443
+read -p "Enter user: " domain
+sudo certbot certonly --standalone --config-dir /usr/local/bin/mediamtx/certbot -d domain
